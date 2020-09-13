7efd916aa64bf8f5316abf5e36928eb2af15546e
Lhoussaine <bouaik.lhou@gmail.com>
2020-09-13 13:31:11 +0100

    Make logic fo switching tabs and start menu page

--- src/index.js
+++ src/index.js
@@ -2,9 +2,26 @@ import './styles/main.scss'
 
 import navbar from './js/navbar'
 import home from './js/home'
+import menu from './js/menu'
 
 const content = document.getElementById('content')
 
 content.appendChild(navbar)
 content.appendChild(home)
 
+
+const links = document.querySelectorAll('.nav-link');
+
+links.forEach(link => link.addEventListener('click', (e) => {
+    let tab = e.target.textContent
+
+    if (tab === "Menu") {
+        content.removeChild(home)
+        content.appendChild(menu)
+    } else if (tab === "Home") {
+        content.appendChild(home)
+        content.removeChild(menu)
+    } else if (tab === "Contact") {
+        
+    }
+}))
---
+++ src/js/menu.js
@@ -0,0 +1,12 @@
+const menu = (() => {
+    const menu = document.createElement('div');
+    menu.className = "menu"
+
+    menu.textContent = "hello form menu"
+
+
+
+
+    return menu
+})();
+export default menu
