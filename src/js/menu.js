190052b1b20f70da28f940a700d8920e08c89622
Lhoussaine <bouaik.lhou@gmail.com>
2020-09-13 14:47:46 +0100

    Create items in menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,4 +1,33 @@
 const menu = (() => {
+    const menuData = [
+        {
+            imageUrl: 'https://img.cuisineaz.com/660x660/2013-12-20/i95731-pizza-royale.jpg',
+            name: "Pizza margaritta",
+            price: "10$"
+        },
+        {
+            imageUrl: 'https://www.biggerbolderbaking.com/wp-content/uploads/2019/07/15-Minute-Pizza-WS-Thumbnail.png',
+            name: "Pizza margaritta",
+            price: "20$"
+        },
+        {
+            imageUrl: 'https://www.biggerbolderbaking.com/wp-content/uploads/2019/07/15-Minute-Pizza-WS-Thumbnail.png',
+            name: "Pizza margaritta",
+            price: "20$"
+        },
+        {
+            imageUrl: 'https://www.biggerbolderbaking.com/wp-content/uploads/2019/07/15-Minute-Pizza-WS-Thumbnail.png',
+            name: "Pizza margaritta",
+            price: "20$"
+        },
+        {
+            imageUrl: 'https://www.biggerbolderbaking.com/wp-content/uploads/2019/07/15-Minute-Pizza-WS-Thumbnail.png',
+            name: "Pizza margaritta",
+            price: "20$"
+        }
+    ]
+
+
     const menu = document.createElement('div');
     menu.className = "menu"
 
@@ -30,16 +59,38 @@ const menu = (() => {
     drinksLink.className = "filter-btn"
     drinksLink.textContent = 'Drinks'
 
-
-
     catrgories.appendChild(pizzaLink)
     catrgories.appendChild(burgerLink)
     catrgories.appendChild(drinksLink)
 
 
+    const menuItems = document.createElement('div');
+    menuItems.className = "menu-items"
+
+    menuData.forEach(item => {
+        const menuItem = document.createElement('div');
+        menuItem.className = "menu-item"
+    
+        const img = document.createElement('img');
+        img.setAttribute('src', item.imageUrl)
+    
+        const nameItem = document.createElement('h4');
+        nameItem.textContent = item.name
+    
+        const priceItem = document.createElement('span');
+        priceItem.textContent = item.price
+    
+        menuItem.appendChild(img)
+        menuItem.appendChild(nameItem)
+        menuItem.appendChild(priceItem)
+    
+    
+        menuItems.appendChild(menuItem)
+    })
+    
+
     album.appendChild(catrgories)
-
-
+    album.appendChild(menuItems)
 
 
     menu.appendChild(menuTitle)
